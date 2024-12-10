b7643f0f67eb0963c21c3467f3995ea1092ab046
isinia <181978133+isinia@users.noreply.github.com>
2024-12-10 01:57:19 +0100

    add OpenWebNet command type and WHO parser

--- oc300.py
+++ oc300.py
@@ -2,19 +2,90 @@
 from argparse import ArgumentParser
 from loguru import logger
 import socket
+import re
 
 OWN_BUFFER_SIZE = 1028
+
+# this list was extracted from the MyHOME Suite binary
+# the only values really being used by the C300 are 7 and 8,
+# but i decided to keep them all for reference
+OWN_WHO_VALUES = {
+    0: 'SCENARIOS',
+    1: 'LIGHTING',
+    2: 'AUTOMATION_SYSTEMS',
+    3: 'APPLIANCES',
+    4: 'HEATING_ADJUSTMENT',
+    5: 'ALARMS',
+    6: 'VIDEO_DOOR_ENTRY_SYSTEM',
+    7: 'MULTIMEDIA',
+    8: 'VIDEO_DOOR_ENTRY_SYSTEM_OVER_IP',
+    9: 'AUXILIARIES',
+    10: 'NAVIGATION',
+    11: 'ENERGY_DISTRIBUTION',
+    13: 'EXTERNAL_INTERFACE_DEVICES',
+    14: 'SPECIAL_COMMANDS',
+    15: 'HOME_AUTOMATION_MAIN_UNIT_COMMAND',
+    16: 'SOUND_SYSTEM',
+    17: 'HOME_AUTOMATION_MAIN_UNIT_MANAGEMENT',
+    99: 'SERVICE_IDENTIFICATION',
+    1001: 'DIAGNOSTIC_OF_LIGHTING_SYSTEM',
+    1004: 'DIAGNOSTIC_OF_HEATING_SYSTEM',
+    1013: 'DIAGNOSTIC_OF_DEVICE_SYSTEM',
+}
+
+OWN_ACK = '*#*1##'
+OWN_NACK = '*#*0##'
+
+# based on the BTicino specification
+# https://developer.legrand.com/uploads/2019/12/OWN_Intro_ENG.pdf
+OWN_CMD_REGEXES = {
+    'NORMAL': r'\*([0-9][0-9]?[0-9]?[0-9]?)\*([0-9#]*)\*([0-9#]*)##',
+    'STATUS_REQUEST': r'\*#([0-9][0-9]?[0-9]?[0-9]?)\*([0-9#]*)##',
+    'DIMENSION_REQUEST': r'\*#([0-9][0-9]?[0-9]?[0-9]?)\*([0-9#]*)\*([0-9#]*)##',
+    'DIMENSION_WRITING': r'\*#([0-9][0-9]?[0-9]?[0-9]?)\*([0-9#]*)\*([0-9#\*]*)##',
+}
+
 OWN_COMMANDS = {
     'monitor_mode_session': '*99*1##',
     'command_mode_session': '*99*0##',
 }
 
-ACK = '*#*1##'
-NACK = '*#*0##'
-
 
 class OpenWebNetNACKException(Exception):
     pass
+
+
+class OpenWebNetCommand:
+    def __init__(self, raw):
+        self.raw = raw
+
+    def _parse_command_syntax(self):
+        if self.raw == OWN_ACK:
+            return ('ACK', ())
+        if self.raw == OWN_NACK:
+            return ('NACK', ())
+
+        for _type in OWN_CMD_REGEXES:
+            match = re.fullmatch(OWN_CMD_REGEXES[_type], self.raw)
+            if match:
+                return (_type, match.groups())
+
+        logger.error(f'failed to parse OpenWebNet command: {self.raw}')
+        return ('UNDEFINED', ())
+
+    def _parse_command_who(self, groups):
+        who = int(groups[0])
+        return OWN_WHO_VALUES[who] if who in OWN_WHO_VALUES else who
+
+    def parse(self):
+        parsed = {}
+        _type, groups = self._parse_command_syntax()
+        parsed['type'] = _type
+
+        if _type not in ('ACK', 'NACK', 'UNDEFINED'):
+            parsed['who'] = self._parse_command_who(groups)
+
+        return parsed
 
 
 class Connection:
@@ -26,7 +97,7 @@ class Connection:
         self.sock = socket.socket()
 
     def _is_nack(self, res):
-        return res == NACK
+        return res == OWN_NACK
 
     def _recv_raw_cmd(self):
         res = self.sock.recv(OWN_BUFFER_SIZE).decode()
@@ -34,7 +105,7 @@ class Connection:
             logger.debug(f'received OpenWebNet command: {res}')
 
         if self._is_nack(res):
-            raise OpenWebNetNACKException(f'received {NACK} response')
+            raise OpenWebNetNACKException(f'received {OWN_NACK} response')
 
         return self.sock.recv(1024).decode()
 
